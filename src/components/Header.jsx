94781ed148f72f0ea503e795f88f3635cbb43fdb
Payal Gurunani <gurunanipayal@gmail.com>
2025-12-07 18:49:28 +0530

    Update logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-white py-6 px-4 shadow-sm w-full">
+  <div className="w-full flex flex-col items-center justify-center ">
+    <img
+          src="/SPY-horizontalfinal1.png"
+      // src="/SPY-horizontalfinal1n.png"
+      // src="/SPY-horizontal.png"
+      alt="SPY Logo"
+      className="h-25 object-cover"
+      aria-label="SPY Logo"
+    />
+  </div>
+</header>
+
+  );
+};
+
+export default Header;
